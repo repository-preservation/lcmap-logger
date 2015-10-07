409ebf86eb3f8c9bd91b0310c754343caed795d2
Duncan M. McGreggor <oubiwann@gmail.com>
2015-10-06 22:29:24 -0500

    Added missing deps.

--- project.clj
+++ project.clj
@@ -9,6 +9,8 @@
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-devel "1.4.0"]
-                 [com.cemerick/friend "0.2.1"]]
+                 [com.cemerick/friend "0.2.1"]
+                 [clojurewerkz/cassaforte "2.0.0"]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :main lcmap-rest.main)
