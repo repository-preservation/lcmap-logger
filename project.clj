7f9bef4d3ccbc5ff2db21c22f8925734293b9002
Duncan McGreggor <oubiwann@gmail.com>
2015-10-09 08:50:44 -0500

    Added client as dependency (to remove duplicate context definitions).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gov.usgs/lcmap-rest "0.1.0-SNAPSHOT"
+(defproject gov.usgs/lcmap-rest "0.1.0-dev"
   :description "LCMAP REST Service API"
   :url "https://github.com/USGS-EROS/lcmap-rest"
   :dependencies [[org.clojure/clojure "1.7.0"]
@@ -13,6 +13,7 @@
                  [ring/ring-defaults "0.1.5"]
                  [com.cemerick/friend "0.2.1"]
                  [clojurewerkz/cassaforte "2.0.0"]
-                 [ch.qos.logback/logback-classic "1.1.3"]]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [gov.usgs/lcmap-client-clj "0.1.0-dev"]]
   :plugins [[lein-ring "0.9.7"]]
   :main lcmap-rest.main)
