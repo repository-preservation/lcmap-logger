562b794ffbacf7fd46933b90911e81353bad1e1c
Duncan McGreggor <oubiwann@gmail.com>
2016-05-19 12:24:15 -0500

    Added logger tests (moved from lcmap.rest).

---
+++ test/lcmap/logger/components/logger_test.clj
@@ -0,0 +1,17 @@
+(ns lcmap.logger.components.logger-test
+  (:require [clojure.test :refer :all]
+            [lcmap.logger.components.logger :as logger]))
+
+(def test-component
+  {:cfg
+    {:lcmap.logging
+      {:level "warn"
+       :namespaces ["lcmap.rest", "lcmap.config", "cassaforte.*"]}}})
+
+(deftest get-log-level-test
+  (is (= :warn (logger/get-log-level test-component))))
+
+(deftest get-namespaces-test
+  (is (= '(lcmap.rest lcmap.config cassaforte.*)
+         (logger/get-namespaces test-component))))
+
